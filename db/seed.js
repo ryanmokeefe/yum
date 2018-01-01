e37ae957a26007203ae1ecc950b85eea1e4be258
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-01 15:04:20 -0500

    added seed data and 2 restaurants

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,43 @@
 var Schema = require("../db/schema.js");
+// define schema models:
+const Restaurant = Schema.Restaurant
+const MenuItem = Schema.MenuItem
+
+
+// clear DBs( no db used, what is being cleared? ) (add error catch):
+Restaurant.remove({}, (err) => {
+    console.log(err)
+})
+
+MenuItem.remove({}, (err) => {
+    console.log(err)
+})
+
+// create some restaurants: 
+// split restaurants/menuItems?
+const restaurant1 = new Restaurant({
+    "name": "Panera",
+    "address": {
+        "street": "123 birch lane",
+        "zipcode": 20010
+    },
+    "yelpUrl": "yelp.com/panera",
+    "items": [
+        new MenuItem({"title": "broccoli cheddar soup"}),
+        new MenuItem({"title": "chicken noodle soup"}),
+        new MenuItem({"title": "grilled cheese"})
+    ]
+})
+const restaurant2 = new Restaurant({
+    "name": "Chipotle",
+    "address": {
+        "street": "567 cedar rd",
+        "zipcode": 20011
+    },
+    "yelpUrl": "yelp.com/chipotle",
+    "items": [
+        new MenuItem({"title": "Chicken burrito"}),
+        new MenuItem({"title": "Steak burrito"}),
+        new MenuItem({"title": "Veggie burrito"})
+    ]
+})
