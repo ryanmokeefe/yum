0f17ff91a5b11012ab6b8522fbe08791368e83d6
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-01 14:46:43 -0500

    set up schemas

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,27 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+// create menuitems schema:
+const MenuItemSchema = new mongoose.Schema({
+    title: String
+})
+//create restaurant schema:
+const RestaurantSchema = new mongoose.Schema({
+    name: String,
+    address: {
+        street: String,
+        zipcode: number
+    },
+    yelpUrl: String,
+    items: [MenuItemSchema]
+})
+// define models from schemas:
+const MenuItemModel = mongoose.model("MenuItemSchema", MenuItemSchema)
+const RestaurantModel = mongoose.model("RestaurantSchema", RestaurantSchema)
+
+// export models:
+module.exports = {
+    Restaurant: RestaurantModel,
+    MenuItem: MenuItemModel
+}
